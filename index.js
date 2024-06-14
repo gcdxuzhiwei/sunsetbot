46ea5706664e2f8ebf293ccf3c11b9b858ec98ff
Xu Zhiwei <gcdxuzhiwei@gmail.com>
2024-06-14 18:06:28 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
 const axios = require("axios");
 
-axios
-  .get(
-    `https://sunsetbot.top/?query_id=${Math.floor(
-      Math.random() * 10000000 + 1
-    ).toString()}&intend=select_city&query_city=%E4%B8%8A%E6%B5%B7&event_date=None&event=set_1&times=None`
-  )
+Promise.all([main("set_1", "今日"), main("set_2", "明日")])
   .then((res) => {
+    sendMsg({
+      title: res[0].title,
+      desp: res[0].desp + res[1].title + res[1].desp,
+    });
+  })
+  .catch((e) => {
+    handleErr(e);
+  });
+
+async function main(event, day) {
+  try {
+    const res = await axios.get(
+      `https://sunsetbot.top/?query_id=${Math.floor(
+        Math.random() * 10000000 + 1
+      ).toString()}&intend=select_city&query_city=%E4%B8%8A%E6%B5%B7&event_date=None&event=${event}&times=None`
+    );
     const { table_content, local_quality, quality_des, local_aod, aod_des } =
       res.data;
-    const msg = {
-      title: `今日火烧云预测:${quality_des}`,
+    return {
+      title: `${day}火烧云预测:${quality_des}`,
       desp: `
 | 北京时间                | 本地预报火烧云鲜艳度 | 本地预报气溶胶 |
 |:------------------------|:---------------------|:---------------|
@@ -21,14 +32,10 @@ axios
 了解详情:[sunsetbot](https://sunsetbot.top/)
 `,
     };
-    console.log("发送的消息", msg);
-    sendMsg(msg);
-  })
-  .catch((e) => {
-    console.log("---error---");
-    console.error(e);
-    console.log("---error---");
-  });
+  } catch (e) {
+    handleErr(e);
+  }
+}
 
 function extractTimeInfo(htmlString) {
   // 定义正则表达式以匹配日期和时间格式
@@ -55,8 +62,12 @@ function sendMsg(msg) {
       })
     )
   ).catch((e) => {
-    console.log("---errorsend---");
-    console.error(e);
-    console.log("---errorsend---");
+    handleErr(e);
   });
+}
+
+function handleErr(e) {
+  console.log("---error---");
+  console.error(e);
+  console.log("---error---");
 }
